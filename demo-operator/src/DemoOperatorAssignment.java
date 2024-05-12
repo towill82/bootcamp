425d6e4daa15068078a13ed54f3ab43e963045a6
william.to <towill@gmail.com>
2024-05-13 00:12:02 +0800

    update

--- demo-operator/src/DemoOperatorAssignment.java
+++ demo-operator/src/DemoOperatorAssignment.java
@@ -3,6 +3,10 @@ public class DemoOperatorAssignment {
     int x = 3;
     int y = (x++ + 3) * x++;
     System.out.println("y=" + y);
+
+    int a = 3;
+    int b = (++a + 3) * ++a;
+    System.out.println("b=" + b);
   }
   
 }
