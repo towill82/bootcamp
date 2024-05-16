e621872b227f15aa35ee4db18509d6dd22ad1ae0
william.to <towill@gmail.com>
2024-05-17 00:55:44 +0800

    update

--- demo-selftesting/src/Demoai.java
+++ demo-selftesting/src/Demoai.java
@@ -1,3 +1,10 @@
 public class Demoai {
-  
+  public static void main(String[] args) {
+      int x = 11;
+      if (x > 10) {
+          System.out.println("x > 10");
+      } else {
+          System.out.println("x < 10");
+      }
+  }
 }
